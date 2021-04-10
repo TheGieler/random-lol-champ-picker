4592b15459932021a561898fcd5b2282f74bf675
TheGieler <giel@raumzeitmedia.de>
2021-04-10 15:42:00 +0200

    Add data structure

--- main.js
+++ main.js
@@ -9,6 +9,53 @@ const champList = [
   "Rengar",
 ];
 
+const runes = [
+  [
+    [1, 2, 3, 4],
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ],
+  [
+    [1, 2, 3, 4],
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ],
+  [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ],
+  [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ],
+  [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3],
+  ],
+];
+
+const stats = [
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+];
+
+const randomRunes = {
+  hauptbaum: [1, 2, 3, 4, 5],
+  hauptbaumrunen: [1, 2, 3, 4],
+  nebenzweig: [1, 2, 3, 4, 5],
+  nebenzweigrunen: [1, 2],
+  stats: [1, 2, 3],
+};
+
 const firstChamp = Math.floor(Math.random() * champList.length);
 const secondChamp = Math.floor(Math.random() * champList.length);
 const thirdChamp = Math.floor(Math.random() * champList.length);
