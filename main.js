0bce83240383841df752044ba4be804c743473b3
TheGieler <giel@raumzeitmedia.de>
2021-04-10 16:29:50 +0200

    Update randomRunes

--- main.js
+++ main.js
@@ -49,25 +49,29 @@ const stats = [
 ];
 
 const randomRunes = {
-  hauptbaum: [1, 2, 3, 4, 5],
-  hauptbaumrunen: [1, 2, 3, 4],
-  nebenzweig: [1, 2, 3, 4, 5],
-  nebenzweigrunen: [1, 2],
-  stats: [1, 2, 3],
+  mainTree: "Dominaz",
+  mainTreeRunes: {
+    keyRune: "",
+    firstMinorRune: "",
+    secondMinorRune: "",
+    thirdMinorRune: "",
+  },
+  offTree: "Zauberei",
+  offTreeRunes: {
+    firstMinorRune: "",
+    secondMinorRune: "",
+  },
+  stats: {
+    firstStat: "",
+    secondStat: "",
+    thirdStat: "",
+  },
 };
 
 const firstChamp = Math.floor(Math.random() * champList.length);
 const secondChamp = Math.floor(Math.random() * champList.length);
 const thirdChamp = Math.floor(Math.random() * champList.length);
 
-// console.log(
-//   champList[firstChamp] +
-//     " " +
-//     champList[secondChamp] +
-//     " " +
-//     champList[thirdChamp]
-// );
-
 function rng() {
-  return Math.floor(Math.random() * Array.length);
+  return Math.floor(Math.random() * this.length);
 }
