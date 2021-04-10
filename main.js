485678fb772e4aa740ef2a30560d717843baf8e6
TheGieler <giel@raumzeitmedia.de>
2021-04-10 15:54:17 +0200

    Add rng function

--- main.js
+++ main.js
@@ -60,10 +60,14 @@ const firstChamp = Math.floor(Math.random() * champList.length);
 const secondChamp = Math.floor(Math.random() * champList.length);
 const thirdChamp = Math.floor(Math.random() * champList.length);
 
-console.log(
-  champList[firstChamp] +
-    " " +
-    champList[secondChamp] +
-    " " +
-    champList[thirdChamp]
-);
+// console.log(
+//   champList[firstChamp] +
+//     " " +
+//     champList[secondChamp] +
+//     " " +
+//     champList[thirdChamp]
+// );
+
+function rng() {
+  return Math.floor(Math.random() * Array.length);
+}
