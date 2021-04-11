b6d5d918566db019b54b266f49813254ab93bf44
TheGieler <giel@raumzeitmedia.de>
2021-04-11 03:37:10 +0200

    Add main rune logic

--- main.js
+++ main.js
@@ -17,13 +17,50 @@ for (let i = 1; i <= 3; i++) {
   console.log(champList[randomNumber]);
 }
 
+const Trees = [
+  [
+    ["Fokusierter Angriff", "Tödliches Tempo", "Leichtfüssigkeit", "Eroberer"],
+    ["Überheilen", "Triumph", "Geistesgegenwart"],
+    ["Legende: Eifer", "Legende: Zähigkeit", "Legende: Blutdurst"],
+    ["Gnadentoss", "Niedermäher", "Letztes Gefecht"],
+  ],
+  [
+    ["Benefee Beschwörung", "Akaner Komet", "Phasenrausch"],
+    ["Kugel der Aufhebung", "Manafluss", "Nimbus-Umhang"],
+    ["Überlegenheit", "Flinkheit", "Absuluter Fokus"],
+    ["Hitzewelle", "Wasserlauf", "Aufziehender Sturm"],
+  ],
+];
+
+console.log("Das sind deine Runen:");
+
+let randomMain = null;
+randomMain = Math.floor(Math.random() * Trees.length);
+
+for (let i = 0; i <= 3; i++) {
+  randomMainMinor = null;
+  randomMainMinor = Math.floor(Math.random() * 3);
+  console.log(Trees[randomMain][i][randomMainMinor]);
+}
+
+console.log("Das sind deine Subrunen:");
+
+let runesSub = null;
+runesSub = Math.floor(Math.random() * Trees.length);
+
+for (let i = 0; i <= 1; i++) {
+  randomSubMinor = null;
+  randomSubMinor = Math.floor(Math.random() * 3);
+  console.log(Trees[runesSub][i][randomSubMinor]);
+}
+
 const stats = [
   ["AD/AP", "Attack Speed", "Ability Haste"],
   ["AD/AP", "Armor", "Magic Resist"],
   ["Life", "Armor", "Magic Resist"],
 ];
 
-const randomStats = [];
+console.log("Deine Random Stats sind:");
 
 for (let i = 1; i <= 3; i++) {
   let randomNumberOne = null;
@@ -32,38 +69,3 @@ for (let i = 1; i <= 3; i++) {
   randomNumberTwo = Math.floor(Math.random() * 3);
   console.log(stats[randomNumberOne][randomNumberTwo]);
 }
-
-const Trees = {
-  Präzision: {
-    keyRune: [
-      "Fokusierter Angriff",
-      "Tödliches Tempo",
-      "Leichtfüssigkeit",
-      "Eroberer",
-    ],
-    firstMinorRune: ["Überheilen", "Triumph", "Geistesgegenwart"],
-    secondMinorRune: [
-      "Legende: Eifer",
-      "Legende: Zähigkeit",
-      "Legende: Blutdurst",
-    ],
-    thirdMinorRune: ["Gnadentoss", "Niedermäher", "Letztes Gefecht"],
-  },
-  Zauberei: {
-    keyRune: ["Benefee Beschwörung", "Akaner Komet", "Phasenrausch"],
-    firstMinorRune: ["Kugel der Aufhebung", "Manafluss", "Nimbus-Umhang"],
-    secondMinorRune: ["Überlegenheit", "Flinkheit", "Absuluter Fokus"],
-    thirdMinorRune: ["Hitzewelle", "Wasserlauf", "Aufziehender Sturm"],
-  },
-};
-
-const randomMainTree = {
-  keyRune: "",
-  firstMinorRune: "",
-  secondMinorRune: "",
-  thirdMinorRune: "",
-};
-const randomSubTree = {
-  firstMinorRune: "",
-  secondMinorRune: "",
-};
