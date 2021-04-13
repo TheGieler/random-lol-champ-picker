841989b817baafd919fa1b4a0959a62d06e2e0e7
TheGieler <giel@raumzeitmedia.de>
2021-04-13 20:57:55 +0200

    Add runeTrees structure

--- main_2_0.js
+++ main_2_0.js
@@ -16,3 +16,33 @@ const champsList = [
     rolls: ["Mage", "Tank"],
   },
 ];
+
+const runeTrees = [
+  {
+    name: "A",
+    keyRune: ["A", "B", "C", "D"],
+    minorRuneRows: [
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+    ],
+  },
+  {
+    name: "B",
+    keyRune: ["A", "B", "C", "D"],
+    minorRuneRows: [
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+    ],
+  },
+  {
+    name: "C",
+    keyRune: ["A", "B", "C"],
+    minorRuneRows: [
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+    ],
+  },
+];
