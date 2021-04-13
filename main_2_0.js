ad325a2664a23e43aa6a43737faf4e486c90d29a
TheGieler <giel@raumzeitmedia.de>
2021-04-13 20:54:26 +0200

    Change object with array

--- main_2_0.js
+++ main_2_0.js
@@ -1,18 +1,18 @@
-const champsList = {
-  champ: {
+const champsList = [
+  {
     name: "Annie",
     rolls: ["Mage"],
   },
-  champ: {
+  {
     name: "Riven",
     rolls: ["Fighter"],
   },
-  champ: {
+  {
     name: "Olaf",
     rolls: ["Tank", "Fighter"],
   },
-  champ: {
+  {
     name: "Galio",
     rolls: ["Mage", "Tank"],
   },
-};
+];
