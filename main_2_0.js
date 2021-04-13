430270b21fb8b4dfeb1473213f217c6cbbb9a977
TheGieler <giel@raumzeitmedia.de>
2021-04-13 20:52:48 +0200

    Add champList struture

---
+++ main_2_0.js
@@ -0,0 +1,18 @@
+const champsList = {
+  champ: {
+    name: "Annie",
+    rolls: ["Mage"],
+  },
+  champ: {
+    name: "Riven",
+    rolls: ["Fighter"],
+  },
+  champ: {
+    name: "Olaf",
+    rolls: ["Tank", "Fighter"],
+  },
+  champ: {
+    name: "Galio",
+    rolls: ["Mage", "Tank"],
+  },
+};
