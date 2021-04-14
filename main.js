04ff12925bc93a44af06b01870f9d07dbc1a7b69
TheGieler <giel@raumzeitmedia.de>
2021-04-14 21:14:08 +0200

    Replace main_2_0.js with main.js

--- main.js
+++ main.js
@@ -1,71 +1,73 @@
-const champList = [
-  "Annie",
-  "Rammus",
-  "Darius",
-  "Diana",
-  "Leona",
-  "Maokai",
-  "Ivern",
-  "Rengar",
+const champsList = [
+  {
+    name: "Annie",
+    rolls: ["Mage"],
+  },
+  {
+    name: "Riven",
+    rolls: ["Fighter"],
+  },
+  {
+    name: "Olaf",
+    rolls: ["Tank", "Fighter"],
+  },
+  {
+    name: "Galio",
+    rolls: ["Mage", "Tank"],
+  },
 ];
 
-console.log("Deine Random Champs sind:");
+const randomChamps = [];
 
 for (let i = 1; i <= 3; i++) {
-  let randomNumber = null;
-  randomNumber = Math.floor(Math.random() * champList.length);
-  console.log(champList[randomNumber]);
-  champList.splice(randomNumber, 1);
+  randomChampPicker(randomChamps);
 }
 
-const Trees = [
-  [
-    ["Fokusierter Angriff", "Tödliches Tempo", "Leichtfüssigkeit", "Eroberer"],
-    ["Überheilen", "Triumph", "Geistesgegenwart"],
-    ["Legende: Eifer", "Legende: Zähigkeit", "Legende: Blutdurst"],
-    ["Gnadentoss", "Niedermäher", "Letztes Gefecht"],
-  ],
-  [
-    ["Benefee Beschwörung", "Akaner Komet", "Phasenrausch"],
-    ["Kugel der Aufhebung", "Manafluss", "Nimbus-Umhang"],
-    ["Überlegenheit", "Flinkheit", "Absuluter Fokus"],
-    ["Hitzewelle", "Wasserlauf", "Aufziehender Sturm"],
-  ],
+const runeTrees = [
+  {
+    name: "A",
+    keyRune: ["A", "B", "C", "D"],
+    minorRuneRows: [
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+    ],
+  },
+  {
+    name: "B",
+    keyRune: ["A", "B", "C", "D"],
+    minorRuneRows: [
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+    ],
+  },
+  {
+    name: "C",
+    keyRune: ["A", "B", "C"],
+    minorRuneRows: [
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+      ["A", "B", "C"],
+    ],
+  },
 ];
-
-console.log("Das sind deine Runen:");
-
-let randomMainTree = null;
-randomMainTree = Math.floor(Math.random() * Trees.length);
-
-for (let i = 0; i <= 3; i++) {
-  randomMainRune = null;
-  runeLenght = Trees[randomMainTree][i].length;
-  randomMainRune = Math.floor(Math.random() * runeLenght);
-  console.log(Trees[randomMainTree][i][randomMainRune]);
-}
-
-console.log("Das sind deine Subrunen:");
-
-let runesSub = null;
-runesSub = Math.floor(Math.random() * Trees.length);
-
-for (let i = 0; i <= 1; i++) {
-  randomSubMinor = null;
-  randomSubMinor = Math.floor(Math.random() * 3);
-  console.log(Trees[runesSub][i][randomSubMinor]);
-}
 
 const stats = [
-  ["AD/AP", "Attack Speed", "Ability Haste"],
-  ["AD/AP", "Armor", "Magic Resist"],
-  ["Life", "Armor", "Magic Resist"],
+  ["A", "B", "C"],
+  ["A", "B", "C"],
+  ["A", "B", "C"],
 ];
 
-console.log("Deine Random Stats sind:");
+function randomChampPicker(arr) {
+  let randomChamp = champsList[randomNumber(champsList)].name;
+  if (arr.includes(randomChamp)) {
+    randomChampPicker(randomChamps);
+  } else {
+    randomChamps.push(randomChamp);
+  }
+}
 
-for (let i = 0; i <= 2; i++) {
-  let randomNumber = null;
-  randomNumber = Math.floor(Math.random() * 3);
-  console.log(stats[i][randomNumber]);
+function randomNumber(arr) {
+  return Math.floor(Math.random() * arr.length);
 }
