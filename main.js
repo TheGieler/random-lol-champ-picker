dad740ad427e188a48320a4a3405947a89e7a0e1
TheGieler <giel@raumzeitmedia.de>
2021-04-17 00:43:30 +0200

    Major changes (Note commit more often)

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
 const champsList = [
   {
     name: "Annie",
-    rolls: ["Mage"],
+    roles: ["Mage"],
   },
   {
     name: "Riven",
-    rolls: ["Fighter", "Assasin"],
+    roles: ["Fighter", "Assasin"],
   },
   {
     name: "Olaf",
-    rolls: ["Tank", "Fighter"],
+    roles: ["Tank", "Fighter"],
   },
   {
     name: "Galio",
-    rolls: ["Mage", "Tank"],
+    roles: ["Mage", "Tank"],
   },
 ];
 
@@ -60,25 +60,95 @@ const stats = [
 
 // Variablen
 
-let userChamp = null;
+let userChampInput = null;
+
+let userChampObject = null;
+
+let userChampIndex = null;
 
 const randomChamps = [];
+const randomChampsName = [];
 const randomStats = [];
 
-// const readline = require("readline");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+let primaryTree = null;
+const randomPrimaryRunes = [];
+
+let secondaryTree = null;
+const randomSecondaryRunes = [];
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // Loops
+
+for (let i = 1; i <= 3; i++) {
+  randomChampPicker(randomChamps);
+}
 
 for (let i = 0; i <= 2; i++) {
   randomStats.push(stats[i][randomNumber(stats)]);
 }
 
-for (let i = 1; i <= 3; i++) {
-  randomChampPicker(randomChamps);
+for (let i = 0; i <= 2; i++) {
+  randomChampsName.push(randomChamps[i].name);
+}
+
+// Start
+
+askQuestion();
+
+function askQuestion() {
+  rl.question(
+    `Your champs are ${randomChampsName.join(", ")} please pick one. \n`,
+    function (answer) {
+      if (randomChampsName.includes(answer)) {
+        userChampInput = answer;
+        console.log(`You picked ${userChampInput} and this are your runes \n`);
+        userChampObject = champsList.find(
+          (champ) => champ.name === userChampInput
+        );
+        userChampIndex = champsList.findIndex((champ) => {
+          return champ.name === userChampInput;
+        });
+
+        randomPrimaryTree();
+
+        console.log(`Your runetree ${runeTrees[primaryTree].name}`);
+
+        for (let i = 0; i <= 3; i++) {
+          randomPrimaryRunes.push(
+            runeTrees[primaryTree].rows[i][
+              randomNumber(runeTrees[primaryTree].rows[i])
+            ]
+          );
+        }
+
+        console.log(` ${randomPrimaryRunes}  \n \n`);
+
+        randomSecondaryTree();
+
+        for (let i = 0; i <= 1; i++) {
+          randomSecondaryRunes.push(
+            runeTrees[secondaryTree].rows[i][
+              randomNumber(runeTrees[secondaryTree].rows[i])
+            ]
+          );
+        }
+
+        console.log(` ${randomSecondaryRunes}  \n`);
+
+        console.log(`Also your random stats are \n \n ${randomStats} \n \n`);
+
+        rl.close();
+      } else {
+        console.error("Please check for a typo");
+        askQuestion();
+      }
+    }
+  );
 }
 
 // Functions
@@ -96,61 +166,50 @@ function randomNumber(arr) {
   return Math.floor(Math.random() * arr.length);
 }
 
-// Start
-
-// askQuestion();
-
-// function askQuestion() {
-//   rl.question(
-//     `Your champs are ${randomChamps.join(", ")} please pick one. \n`,
-//     function (answer) {
-//       if (randomChamps.includes(answer)) {
-//         userChamp = answer;
-//         console.log(`You picked ${userChamp} and this are your runes \n`);
-//         rl.close();
-//       } else {
-//         console.error("Please check for a typo");
-//         askQuestion();
-//       }
-//     }
-//   );
-// }
-
-// Testbereich
-
-primaryTree = null;
-primaryTree = randomNumber(runeTrees);
-randomPrimaryRunes = [];
-
-for (let i = 0; i <= 3; i++) {
-  randomPrimaryRunes.push(
-    runeTrees[primaryTree].rows[i][randomNumber(runeTrees[primaryTree].rows[i])]
-  );
+function randomPrimaryTree() {
+  primaryTree = randomNumber(runeTrees);
+  roleCheck(primaryTree, randomPrimaryTree);
 }
 
-SecondaryTree = null;
-randomSecondaryRunes = [];
-randomSecondaryTree();
-
 function randomSecondaryTree() {
-  SecondaryTree = randomNumber(runeTrees);
-  if (SecondaryTree === primaryTree) {
+  secondaryTree = randomNumber(runeTrees);
+  if (secondaryTree === primaryTree) {
     randomSecondaryTree();
   }
 }
 
-for (let i = 0; i <= 1; i++) {
-  randomSecondaryRunes.push(
-    runeTrees[SecondaryTree].rows[i][
-      randomNumber(runeTrees[SecondaryTree].rows[i])
-    ]
-  );
-}
+function roleCheck(tree, func) {
+  const runeRoles = runeTrees[tree].roles;
+  const champRoles = champsList[userChampIndex].roles;
 
-function rollCheck() {
-  if (
-    runeTrees[primaryTree].rolls.includes(champsList[randomChampIndex].rolls)
-  ) {
-    rollCheck();
+  if (champRoles.some((champRole) => runeRoles.includes(champRole))) {
+    func();
   }
 }
+
+// Lang
+
+// const runeRoles = runeTrees[0].roles;
+// const champRoles = champsList[0].roles;
+
+// function checkRole(func) {
+//   for (let i = 0; i < champRoles.length; i++) {
+//     const champRole = champRoles[i];
+
+//     for (let j = 0; j < runeRoles.length; j++) {
+//       const runeRole = runeRoles[j];
+
+//       if (champRole === runeRole) {
+//         func();
+//       }
+//     }
+//   }
+// }
+
+// Kurz
+
+// const champRoles = champsList[0].roles;
+
+// const champHasMatchingRunes3 = champRoles.some((champRole) =>
+//   runeRoles.includes(champRole)
+// );
